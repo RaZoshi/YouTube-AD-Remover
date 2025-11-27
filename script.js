9e60ab8d1a324472149dfd3643e2cfd279869187
razoshi <141675594+RaZoshi@users.noreply.github.com>
2025-11-28 00:04:44 +0300

    Create script.js
    Ctrl + C, Ctrl + V in your browser script manager, and go to youtube

---
+++ script.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         YouTube (Removed AD from main page)
+// @namespace    http://tampermonkey.net/
+// @version      2025-11-27
+// @description  https://github.com/RaZoshi
+// @author       razoshi
+// @match        https://www.youtube.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const classes = ["style-scope ytd-in-feed-ad-layout-renderer"];
+    const ids = ["rendering-content"];
+
+    function removeAds() {
+        classes.forEach(cls => {
+            const ads = document.querySelectorAll(`.${cls.split(' ').join('.')}`);
+            ads.forEach(ad => ad.remove());
+        });
+
+        ids.forEach(id => {
+            const el = document.getElementById(id);
+            if (el) el.remove();
+        });
+    }
+
+    removeAds();
+
+    const observer = new MutationObserver(() => {
+        removeAds();
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+})();
