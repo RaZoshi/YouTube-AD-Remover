ff4239a8c923d92dc9033022af64c66218be0fb6
razoshi <141675594+RaZoshi@users.noreply.github.com>
2025-11-28 00:06:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         YouTube (AD Remover)
+// @name         YouTube (AD-Remover)
 // @namespace    http://tampermonkey.net/
 // @version      2025-11-27
 // @description  https://github.com/RaZoshi
